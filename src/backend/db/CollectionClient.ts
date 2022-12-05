37ec2aeb151dd79b74ff88c5ac4136b2143179ea
Miguel Silva <miguelsilvavianaa@gmail.com>
2022-12-05 01:28:59 -0300

    integration with backend firebase

--- src/backend/db/CollectionClient.ts
+++ src/backend/db/CollectionClient.ts
@@ -1,71 +1,44 @@
-import { dataBase } from '../config'
-import firestore, {
-  addDoc,
-  collection,
-  deleteDoc,
-  doc,
-  getDoc,
-  getDocs,
-  setDoc,
-} from 'firebase/firestore'
-import Client from '../../core/Client'
-import ClientRepository from '../../core/ClientRepository'
- 
+import ClientRepository from "../../core/ClientRepository";
+import Client from "../../core/Client";
+import firebase from '../config'
+
+
 export default class CollectionClient implements ClientRepository {
 
-  save(client: Client): Promise<Client> {
-      throw new Error('Method not implemented.')
-  }
-  del(client: Client): Promise<null> {
-      throw new Error('Method not implemented.')
-  }
-  getAll(client: Client): Promise<Client[]> {
-      throw new Error('Method not implemented.')
-  }
- 
-  #converter = {
-    toFirestore: (cliente: Client) => {
-      return {
-        nome: cliente.name,
-        idade: cliente.age,
-      }
-    },
-    fromFirestore: (
-      snapshot: firestore.QueryDocumentSnapshot,
-      options: firestore.SnapshotOptions,
-      ) => {
-        const data = snapshot.data(options)
-        return new Client(data.name, data.age, snapshot.id)
-      },
+    #converter = {
+        toFirestore(client: Client) {
+            return {
+                name: client.name,
+                age: client.age,
+            }
+        },
+        fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions): Client {
+            const data = snapshot.data(options)
+            return new Client(data.name, data.age, snapshot.id)
+        }
     }
-    
-  #colecaoCliente = collection(dataBase, 'clientes').withConverter(this.#converter)
- 
-  async salvar(client: Client): Promise<Client> {
-    if (client?.id) {
-      await setDoc(
-        doc(dataBase, 'clientes', client.id).withConverter(this.#converter),
-        client,
-      )
-      return client
-    } else {
-      const docRef = await addDoc(
-        this.#colecaoCliente,
-        client,
-      )
-      const doc = await getDoc(docRef)
-      return doc.data()
+
+    async save(client: Client): Promise <Client> {
+        if(client?.id) {
+           await this.collection().doc(client.id).set(client)
+           return client
+        } else {
+           const docRef = await this.collection().add(client)
+           const doc = await docRef.get()
+           return doc.data()
+        }
     }
-  }
- 
-  async excluir(client: Client): Promise<void> {
-    return await deleteDoc(doc(dataBase, 'clients', client.id))
-  }
- 
-  async obterTodos(): Promise<Client[]> {
-    const clientesCol = this.#colecaoCliente
-    const clientesSnapshot = await getDocs(clientesCol)
-    const clientesList = clientesSnapshot.docs.map((doc) => doc.data()) ?? []
-    return clientesList
-  }
+
+    async del(client: Client): Promise<void> {
+        return this.collection().doc(client.id).delete()
+    }
+
+    async getAll(): Promise<Client[]> {
+        const query = await this.collection().get()
+        return query.docs.map(doc => doc.data()) ?? []
+    }
+
+    private collection() {
+        return firebase.firestore().collection('clients').withConverter(this.#converter)
+    }
 }
